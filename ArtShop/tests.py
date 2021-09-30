5fb705ba6f6c825a8197d1bfb88b106d4479b53d
Jacob Pagan <jake2415@gmail.com>
2021-09-29 19:53:53 -0500

    Done for the second day. Started writing the basic unit tests.

--- ArtShop/tests.py
+++ ArtShop/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
+from .models import Category, ArtPiece
 
-# Create your tests here.
+
+class TestArtShop(TestCase):
+    '''
+    TODO:
+        - Add tests for the following:
+        - Models
+        - URLS
+        - Add mutations to the schema
+    '''
+    def setUp(self):
+        '''
+        Create a category and art piece for testing
+        '''
+        self.category = Category.objects.create(name='Test Category')
+        self.art_piece = ArtPiece.objects.create(
+            name='Test Art Piece',
+            description='Test Description',
+            category=self.category,
+            price=100.00)
+
+    # def test_that_fails(self):
+    #     self.assertTrue(False)
+
+    def test_that_passes(self):
+        self.assertEqual(1 + 1, 2)
+
+    def test_category_name_is_valid_true(self):
+        test_category = Category.objects.filter(name='Test Category')
+        print(test_category[0])
+        self.assertTrue(test_category.exists())
+
+    def test_category_name_is_valid_false(self):
+        test_category = Category.objects.filter(name='Test False')
+        self.assertFalse(test_category.exists())
+
+    def tearDown(self):
+        '''
+        Delete the category and art piece created for testing
+        '''
+        self.category.delete()
+        self.art_piece.delete()
--- daniels_site/schema.py
+++ daniels_site/schema.py
@@ -14,7 +14,7 @@ class ArtPieceType(DjangoObjectType):
     class Meta:
         model = ArtPiece
         #Not sure I'm going to use the image field but I'm keeping it here for now
-        fields = ('id', 'title', 'description', 'price', 'category')
+        fields = ('id', 'name', 'description', 'price', 'category')
 
 
 class Query(graphene.ObjectType):
@@ -22,9 +22,9 @@ class Query(graphene.ObjectType):
     art_pieces = graphene.List(ArtPieceType)
     art_pieces_by_name = graphene.List(ArtPieceType, name=graphene.String())
     art_pieces_by_category = graphene.List(ArtPieceType,
-                                           category=graphene.String())
+                                           name=graphene.String())
 
-    def resolve_category(self, info, name, **kwargs):
+    def resolve_category_by_name(self, info, name, **kwargs):
         try:
             return Category.objects.get(name=name)
         except Category.DoesNotExist:
@@ -32,6 +32,12 @@ class Query(graphene.ObjectType):
 
     def resolve_art_pieces(self, info, **kwargs):
         return ArtPiece.objects.select_related('category').all()
+
+    def resolve_art_pieces_by_name(self, info, name, **kwargs):
+        return ArtPiece.objects.filter(name__icontains=name)
+
+    def resolve_art_pieces_by_category(self, info, name, **kwargs):
+        return ArtPiece.objects.filter(category__name__icontains=name)
 
 
 schema = graphene.Schema(query=Query)
