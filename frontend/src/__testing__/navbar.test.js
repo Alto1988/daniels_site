89afbe3d5699d4a06e5a803d4822933a4c3e7f45
Jacob Pagan <jake2415@gmail.com>
2021-10-26 17:09:38 -0500

    Started the frontend testing but still getting used to jest

---
+++ frontend/src/__testing__/navbar.test.js
@@ -0,0 +1,9 @@
+import { About } from "../components/Navigation/about";
+
+// test("should return text", () => {
+//   render(<About />).should.contain(<h4>About</h4>);
+// });
+
+test("Should return true", () => {
+  expect(true).toBe(true);
+});
